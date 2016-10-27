9e39b458e7ad710e5669ad31b473eac502e16080
StreetMaster <supporto@streetmaster.it>
2016-10-27 17:23:17 +0200

    comment

--- VerifyRest_ASPNET/DemoVerify.aspx.cs
+++ VerifyRest_ASPNET/DemoVerify.aspx.cs
@@ -8,7 +8,7 @@ namespace VerifyRest_ASPNET
     /// realizzato da StreetMaster Italia
     /// 
     /// L'end point del servizio è 
-    ///     http://ec2-46-137-97-173.eu-west-1.compute.amazonaws.com/smrest/verify
+    ///     http://ec2-46-137-97-173.eu-west-1.compute.amazonaws.com/smrest/webresources/verify
     ///     
     /// Per l'utilizzo registrarsi sul sito http://streetmaster.it e richiedere la chiave per il servizio VERIFY 
     /// Il protocollo di comunicazione e' in formato JSON
